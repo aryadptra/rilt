b615b1b4d60fc45f7b283c58331d64d7f2b39af2
aryadwiputra <aryadptr.business@gmail.com>
2024-05-07 11:17:15 +0700

    Configure home page

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import AppLayout from '@/Layouts/AppLayout'
+
+const Home = () => {
+    return (
+        <>
+            Home
+        </>
+    )
+}
+
+Home.layout = page => <AppLayout children={page} title="Inertia" />
+
+export default Home
