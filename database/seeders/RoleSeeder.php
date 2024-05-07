6f94429ff47d0463fc6c86a9cc8413cfba9c0e19
aryadwiputra <aryadptr.business@gmail.com>
2024-05-07 11:48:06 +0700

    Create seeders

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $user = User::factory()->create([
+            'name' => 'Arya Dwi Putra',
+            'email' => 'arya@aryadwiputra.com',
+        ]);
+
+        // Membuat role 'admin' dan 'writer'
+        $roles = collect([
+            ['name' => 'admin'],
+            ['name' => 'writer'],
+        ])->map(function ($role) {
+            return Role::create($role);
+        });
+
+        // Menautkan role 'admin' ke user
+        $user->roles()->attach($roles->first()->id);
+    }
+}
